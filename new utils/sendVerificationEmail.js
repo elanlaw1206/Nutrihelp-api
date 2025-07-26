4562111d0ca675a50323847b56e162ad13353529
elanlaw <elanlaw1206@gmail.com>
2025-07-26 20:48:04 +1000

    Initial placeholder for email verification utility

---
+++ new utils/sendVerificationEmail.js
@@ -0,0 +1,9 @@
+// sendVerificationEmail.js
+// This function will handle sending verification emails to new users.
+// TODO: Implement actual email sending logic using nodemailer or Supabase SMTP
+
+async function sendVerificationEmail(email, token) {
+  console.log(`Pretend to send email to ${email} with token ${token}`);
+}
+
+module.exports = sendVerificationEmail;
